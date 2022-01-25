b248d49ac9ef421c3ca7ada6f5fa7c68e22e69dc
kheireddine-Hdr <kheirddine.haddour@yahoo.fr>
2022-01-25 13:07:17 +0100

    first commit

---
+++ templates/accueil.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include('head.php');
+include('menu.php');?>
+
+
+<div class='accueil'>
+    <div id="presentation">
+       <h1> <strong>Bienvenue dans la cuisine méditerranéenne</strong></h1>
+       <p>LE Méditerranéen Délice est un site participatif, conçu par les étudiants de la faculté des sciences pour les amoureux de la cuisine méditerranéenne.</p>
+       <p>Vous avez la possibilité de consulter les recettes ajoutées par les participants, d'en ajouter des nouvelles ou en supprimer une/des recette(s) qui n'est / ne sont plus à votre gout.</p>
+
+</div>
+</div>
+
+<?php
+include('footer.php');
+?>
+
+
---
+++ templates/footer.php
@@ -0,0 +1,13 @@
+</body>
+<footer>
+	<nav>
+		<ul>
+                    <li><a href="" >a| </a></li>
+                      
+                    <li><a href=""> b | </a></li>
+                    <li><a href="">c</a></li>
+                    <li><a href="https://fr.wikipedia.org/wiki/Cuisine_méditerranéenne"> ©2020  </a></li>
+               </ul>
+        </nav>
+</footer>
+</html>
